00d1a1ffb3153422051dc76f7f794190ef738aee
kenken64 <bunnyppl@hotmail.com>
2024-09-29 00:56:35 +0800

    upd

---
+++ casinoapp/src/main/java/org/allinroyale/casinoapp/CasinoappApplication.java
@@ -0,0 +1,13 @@
+package org.allinroyale.casinoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoappApplication.class, args);
+	}
+
+}
---
+++ casinoapp/src/test/java/org/allinroyale/casinoapp/CasinoappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.allinroyale.casinoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
